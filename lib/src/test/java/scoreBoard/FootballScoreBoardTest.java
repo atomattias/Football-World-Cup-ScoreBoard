f7b3c708d54607426d4212fc8156b3bf14ca498b
atomattias <atomattias@gmail.com>
2023-08-03 12:48:28 +0200

    RED: Add failing test for FootballScoreBoard class for starting game and add to score board

--- lib/src/test/java/scoreBoard/FootballScoreBoardTest.java
+++ lib/src/test/java/scoreBoard/FootballScoreBoardTest.java
@@ -1,4 +1,28 @@
 package scoreBoard;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 public class FootballScoreBoardTest {
+    private FootballScoreBoard scoreBoard;
+    private FootballTeam teamA;
+    private FootballTeam teamB;
+
+    @BeforeEach
+    public void setup() {
+        scoreBoard = new FootballScoreBoard();
+
+        teamA = new FootballTeam("Canada");
+        teamB = new FootballTeam("Spain");
+    }
+
+    @Test
+    public void testStartGameAddsGameToScoreBoard() {
+        scoreBoard.startGame(teamA, teamB);
+
+        assertEquals(1, scoreBoard.getGames().size());
+        FootballGame game = scoreBoard.getGames().get(0);
+        assertEquals(teamA, game.getHomeTeam());
+        assertEquals(teamB, game.getAwayTeam());
+    }
 }
