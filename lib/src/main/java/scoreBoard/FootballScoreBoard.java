2a3f2ef4cfd9e6abf8ac6b75bef7de43f812114f
atomattias <atomattias@gmail.com>
2023-08-03 12:56:46 +0200

    GREEN: Implement FootballScoreBoard class startGame method

--- lib/src/main/java/scoreBoard/FootballScoreBoard.java
+++ lib/src/main/java/scoreBoard/FootballScoreBoard.java
@@ -1,4 +1,23 @@
 package scoreBoard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FootballScoreBoard {
+    private List<FootballGame> games;
+
+    public FootballScoreBoard() {
+        this.games = new ArrayList<>();
+    }
+    public List<FootballGame> getGames() {
+        return games;
+    }
+    public void startGame(FootballTeam homeTeam, FootballTeam awayTeam) {
+        if (homeTeam.equals(awayTeam)) {
+            throw new IllegalArgumentException("Home and away teams must be different.");
+        }
+
+        FootballGame newGame = new FootballGame(homeTeam, awayTeam);
+        games.add(newGame);
+    }
 }
